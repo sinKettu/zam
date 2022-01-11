3c49fd4ab60b0bf842d8a832a15693001171870d
sinkettu <avangard.jazz@gmail.com>
2022-01-11 10:29:51 +0300

    Initial

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+pub use reqwest::{
+    self,
+    header::{
+        HeaderMap,
+        HeaderName,
+        HeaderValue
+    },
+    redirect::Policy,
+    blocking::Response
+};
+
+fn main() {
+    println!("Hello, world!");
+}
